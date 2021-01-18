4d8bd139d1c1cdc16f5eb9f35074b3c8e98f2298
johnklima <klima.de.sa@gmail.com>
2021-01-18 13:12:54 +0000

    add drag and move

--- Assets/IKSegment.cs
+++ Assets/IKSegment.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class IKSegment : MonoBehaviour
 {
+
+    public float length = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +16,10 @@ public class IKSegment : MonoBehaviour
     public void drag(Vector3 target)
     {
         //simply looks at target
-        transform.LookAt(target);       
+        transform.LookAt(target);
 
-
+        //next move too
+        transform.position = target - transform.forward * length;
     }
 
 
