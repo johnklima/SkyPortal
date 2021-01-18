7204db990c4fc5526aa7dca6c8c98e0e7cefb922
johnklima <klima.de.sa@gmail.com>
2021-01-18 12:58:53 +0000

    array first

--- Assets/IKSystem.cs
+++ Assets/IKSystem.cs
@@ -5,8 +5,24 @@ using UnityEngine;
 public class IKSystem : MonoBehaviour
 {
     public IKSegment[] segments;
-    // Start is called before the first frame update
-    void Start()
+
+    int childcount;
+    void Awake()    {
+
+        //lets buffer our segements in an array
+        childcount = transform.childCount;
+        segments = new IKSegment[childcount];
+        int i = 0;
+        foreach (Transform seg in transform)
+        {
+            segments[i] = seg.GetComponent<IKSegment>();
+            i++;
+        }
+
+    }
+
+        // Start is called before the first frame update
+        void Start()
     {
         
     }
