eca85fd82c4aa76cff69c5a2eef2a2e9354bebfd
johnklima <klima.de.sa@gmail.com>
2021-01-18 13:50:11 +0000

    final ik working

--- Assets/IKSegment.cs
+++ Assets/IKSegment.cs
@@ -7,6 +7,12 @@ public class IKSegment : MonoBehaviour
 {
 
     public float length = 3.0f;
+    public IKSegment parent = null;
+    public IKSegment child = null;
+
+    public Vector3 Apos = new Vector3(0, 0, 0);
+    public Vector3 Bpos = new Vector3(0, 0, 0);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +24,47 @@ public class IKSegment : MonoBehaviour
         //simply looks at target
         transform.LookAt(target);
 
-        //next move too
+        //next move to
         transform.position = target - transform.forward * length;
+
+        //inform my parent
+        if (parent)
+            parent.drag(transform.position);
+    }
+    public void reach(Vector3 target)
+    {
+        drag(target);
+        updateSegment();
     }
 
+    public void updateSegmentAndChildren()
+    {
 
+        updateSegment();
+
+        //update its children
+        if (child)
+            child.updateSegmentAndChildren();
+    }
+
+    public void updateSegment() 
+    {
+        if (parent)
+        {
+            Apos = parent.Bpos;         //could also use parent endpoint...
+            transform.position = Apos;  //move me to Bpos (parent endpoint)
+        }
+        else
+        {
+            //Apos is always my position
+            Apos = transform.position;
+        }
+        //Bpos is always where the endpoint will be, as calculated from length 
+        calculateBpos();
+
+    }
+    void calculateBpos()
+    {
+        Bpos = Apos + transform.forward * length;
+    }
 }
--- Assets/IKSystem.cs
+++ Assets/IKSystem.cs
@@ -27,6 +27,20 @@ public class IKSystem : MonoBehaviour
         firstSegment = segments[0];
         lastSegment = segments[childcount - 1];
 
+        for(int c = 1; c < childcount; c++)
+        {
+            segments[c].parent = segments[c - 1];
+
+            if (c < childcount - 2)
+            {
+                segments[c].child = segments[c + 1];
+            }
+
+        }
+        //now assign c == 0
+        segments[0].child = segments[1];
+
+
     }
 
     // Start is called before the first frame update
@@ -38,6 +52,20 @@ public class IKSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        lastSegment.drag(target.position);
+        //if you want simple drag, uncomment below, and comment reach
+        //lastSegment.drag(target.position);
+
+        //call reach on the last
+        lastSegment.reach(target.position);
+
+        //and forward update on the first
+        //we needed to maintain that first segment original position
+        //which is the position of the IK system itself
+
+        //COMMENT NEXT LINE AND IK ROOT WILL FOLLLOW TARGET:
+        firstSegment.transform.position = transform.position;
+
+        firstSegment.updateSegmentAndChildren();
+
     }
 }
