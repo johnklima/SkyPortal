eb00a828a8d81468706d8531dcae980e235038ab
MarkusKamp <72786566+MarkusKamp@users.noreply.github.com>
2021-01-19 12:29:20 +0100

    Added box with script

---
+++ Assets/Markus_Kamphaug.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Markus_Kamphaug : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
