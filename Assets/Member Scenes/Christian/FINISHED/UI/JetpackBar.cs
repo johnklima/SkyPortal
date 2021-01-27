89550105b6c82747c95ed3ad3f04b575eb23f98f
GzSmile <Christian.kollbaer@gmail.com>
2021-01-27 15:14:34 +0100

    AddedGameHudForJetpack

---
+++ Assets/Member Scenes/Christian/FINISHED/UI/JetpackBar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JetpackBar : MonoBehaviour
+{
+   public Image _Image;
+   public GameObject player;
+   private PlayerScript playerController;
+   public float maximum;
+   public float minimum = 0f;
+   public float current;
+
+   private void Start()
+   {
+      playerController = player.GetComponent<PlayerScript>();
+   }
+
+   private void Update()
+   {
+      maximum = playerController.maxFuel;
+      minimum = 0f;
+      current = playerController.fuel;
+
+      float currentOffset = current - minimum;
+      float maximumOffset = maximum - minimum;
+      float fillAmount = currentOffset / maximumOffset;
+      _Image.fillAmount = fillAmount;
+   }
+}
