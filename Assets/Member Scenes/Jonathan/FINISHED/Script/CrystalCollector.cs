fbf932c1e8bf5c4d5572d0b374d6e602b74fb306
JonathanGuasco <62176506+JonathanGuasco@users.noreply.github.com>
2021-02-11 13:06:41 +0100

    Particle Script Edit

--- Assets/Member Scenes/Jonathan/FINISHED/Script/CrystalCollector.cs
+++ Assets/Member Scenes/Jonathan/FINISHED/Script/CrystalCollector.cs
@@ -8,24 +8,30 @@ public class CrystalCollector : MonoBehaviour
 {
     private UIManager _uiManager;
     public AudioSource collectSound;
+    public ParticleSystem collectCrystals;
 
 
     private void Start()
     {
-        _uiManager = GameObject.FindWithTag("UIManager").GetComponent<UIManager>(); // This caches the UI Manager so we can reference the script variables
+        //_uiManager = GameObject.FindWithTag("UIManager").GetComponent<UIManager>(); // This caches the UI Manager so we can reference the script variables
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player") && Input.GetKey(KeyCode.F))
         {
-            _uiManager.crystalsCollected++; // Adds to our counter in UI Manager
+            //_uiManager.crystalsCollected++; // Adds to our counter in UI Manager
+            collect();
             Destroy(gameObject); // Destroy's the object - worth to note that if it was a bigger project object pooling is king.
             collectSound.Play();
         }
 
-        
+
         //if(other.CompareTag("Player")) This is faster due to not needing to check another GameObject for its tag it simply compares the current trigger's tag with a string
         //if(other.gameObject.tag == "Player")
+    }
+    private void collect()
+    {
+        collectCrystals.Play();
     }
 }
