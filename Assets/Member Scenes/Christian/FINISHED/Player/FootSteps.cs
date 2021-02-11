4e31d91606f3e3b2cd37864cbcca61aa4ed31e62
TheDevArray <Christian.kollbaer@gmail.com>
2021-02-11 01:06:47 +0100

    Walking and Running sound script

--- Assets/Member Scenes/Christian/FINISHED/Player/FootSteps.cs
+++ Assets/Member Scenes/Christian/FINISHED/Player/FootSteps.cs
@@ -1,21 +1,83 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FootSteps : MonoBehaviour
 {
-    // Start is called before the first frame update
-    
-    
-    
-    void Start()
+    public GameObject player;
+    private PlayerScript ps;
+    public AudioClip[] clips;
+    public AudioSource ac;
+    private bool couroutineOn;
+    private float stepDelay;
+    public float walkDelay;
+    public float runDelay;
+    public float countDown;
+    public float stopWalkSoundSpeed;
+
+
+    private void Start()
     {
-        
+        int rand = Random.Range(0, 1);
+        ps = player.GetComponent<PlayerScript>();
+        couroutineOn = true;
+        ac.clip = clips[rand];
+
+        StartCoroutine(Walking());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (ps.isGrounded)
+        {
+            countDown = 1f;
+        }
+        else
+        {
+            if (countDown > 0)
+            {
+                countDown -= Time.deltaTime * stopWalkSoundSpeed;
+            }
+        }
+
+        if (countDown < 0)
+        {
+            ac.Stop();
+        }
     }
+
+    IEnumerator Walking()
+    {
+
+        while (couroutineOn)
+        {
+
+            if (ps.inputDir.magnitude > 0 && countDown > 0)
+            {
+                int rand = Random.Range(0, 1);
+                
+                if (ps.isRunning)
+                {
+                    stepDelay = runDelay;
+                    ac.volume = Random.Range(0.9f, 1f);
+                    ac.pitch = Random.Range(1f, 1.2f);
+                    ac.clip = clips[rand];
+                }
+                else
+                {
+                    stepDelay = walkDelay;
+                    ac.volume = Random.Range(0.8f, 1f);
+                    ac.pitch = Random.Range(0.8f, 1f);
+                    ac.clip = clips[rand];
+                    
+                }
+
+                ac.Play();
+                
+            }
+            
+            yield return new WaitForSeconds(stepDelay);
+            
+        }
+    }
+
 }
